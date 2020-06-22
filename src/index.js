3877be67ecb1d205399db6015c619a730afd224a
Aman Kumar Srivastava <33771267+amansri2502@users.noreply.github.com>
2020-06-23 04:58:47 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios'
 
-axios.defaults.baseURL="https://burger-7ef95.firebaseio.com/";
+axios.defaults.baseURL="https://burger.firebaseio.com/";
 
 ReactDOM.render(
   <React.StrictMode>
